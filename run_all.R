48d3ae7d2b7cc53d670642bd898027ab8477d608
Douglas Munro <84464349+douglasmunro@users.noreply.github.com>
2024-09-02 13:26:16 +0100

    Update run_all.R

--- run_all.R
+++ run_all.R
@@ -11,15 +11,13 @@ library(dplyr)
 # load functions
 source("./R/05_constituency_map.R")
 
-# load new Lib Dem constituencies 
-new_seats <- read_csv("./data/constituencies.csv", 
-                     col_select = c(name, new_mp)) |> 
-  filter(new_mp == TRUE) |> 
-  select(!new_mp) |> 
-  pull()
+# load Lib Dem constituencies 
+seats <- read_csv("./data/constituencies.csv", 
+                     col_select = c(name)) |> 
+   pull()
 
 # create a map for each constituency                      
-for (seat in new_seats) {
+for (seat in seats) {
   constituency_map(seat)
 }
 
